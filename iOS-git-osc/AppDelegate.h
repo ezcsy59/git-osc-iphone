12f51002439485e0968beabd3f419ed10fc69dcd
ezcsy59 <chensiyu@chensiyudeMacBook-Air.local>
2015-09-22 11:53:06 +0800

    1

---
+++ iOS-git-osc/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  iOS-git-osc
+//
+//  Created by chenhaoxiang on 14-5-4.
+//  Copyright (c) 2014年 chenhaoxiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MenuViewController.h"
+#import "ProjectsViewController.h"
+#import "PKRevealController.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
